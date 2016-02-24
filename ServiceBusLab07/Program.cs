9d9945aeef2469c899729fab9be0f91d551b7216
Jperezaguila <jperezaguila@yahoo.es>
2016-02-24 12:29:42 +0100

    Funcional

---
+++ ServiceBusLab07/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.ServiceModel;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.ServiceBus;
+
+namespace ServiceBusLab07
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var host=new ServiceHost(typeof(ServicioSaludo));
+            host.AddServiceEndpoint(typeof (IServicioSaludo),
+                new NetTcpBinding(), "net.tcp://localhost:6985/saludo");
+
+            host.AddServiceEndpoint(typeof(IServicioSaludo), 
+                new NetTcpRelayBinding(), 
+                ServiceBusEnvironment.CreateServiceUri
+                ("sb", "namespacelab07", "saludo")).Behaviors.Add(new TransportClientEndpointBehavior()
+            {
+                    TokenProvider = TokenProvider.CreateSharedSecretTokenProvider("owner", "Lkulzq9ddCEIT53k0cvGuE33+mFB2K5ppyyRivkww2E=")
+            });
+            host.Open();
+            Console.WriteLine("Terminar...");
+            Console.ReadLine();
+            host.Close();
+        }
+    }
+}
---
+++ ServiceBusLab07/ServicioSaludo.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.Text;
+
+namespace ServiceBusLab07
+{
+    // NOTE: You can use the "Rename" command on the "Refactor" menu to change the class name "ServicioSaludo" in both code and config file together.
+    public class ServicioSaludo : IServicioSaludo
+    {
+
+        private Dictionary<String, String> Saludos = new Dictionary<string, string>()
+        {
+            {"Español", "Buenos días"},
+            {"Ingles", "Good morning"},
+            {"Frances", "Bon jour"},
+            {"Aleman", "Guten morgen"}
+        };
+
+      
+        public string GetSaludo(string idioma)
+        {
+            if (Saludos.ContainsKey(idioma))
+                return Saludos[idioma];
+            return "idioma no disponible";
+
+        }
+
+        public void Dispose()
+        {
+            
+        }
+    }
+}
